7ca1b56ca59180152c89c88f660411baad5cd20f
Janzen Zarzoso <janzen.zarzoso@gmail.com>
2017-01-12 18:14:47 +0800

    Add actions.

---
+++ src/actions.js
@@ -0,0 +1,37 @@
+/**
+ * Actions - are payloads of information that send data from your application to your store. Actions can be divided into 2 functions:
+ * - ActionTypes - are just constants to identify Actions.
+ * - ActionCreators - are functions that create an Action.
+ *   - You can use `bindActionCreators()` to automatically bind to a `dispatch()` function.
+ */
+
+/*
+ * Action Types
+ */
+export const ADD_TODO = 'ADD_TODO';
+export const TOGGLE_TODO = 'TOGGLE_TODO'
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
+
+/*
+ * Other constants
+ */
+export const VisibilityFilters = {
+	SHOW_ALL: 'SHOW_ALL',
+	SHOW_COMPLETED: 'SHOW_COMPLETED',
+	SHOW_ACTIVE: 'SHOW_ACTIVE'
+}
+
+/*
+ * Action creators
+ */
+export function addTodo(text) {
+	return { type: ADD_TODO, text }
+}
+
+export function toggleTodo(index) {
+  return { type: TOGGLE_TODO, index }
+}
+
+export function setVisibilityFilter(filter) {
+  return { type: SET_VISIBILITY_FILTER, filter }
+}
