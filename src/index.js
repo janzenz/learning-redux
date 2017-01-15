ebc8e5c3e0b869a3430dbe4a4013624f670a2d4a
Janzen Zarzoso <janzen.zarzoso@gmail.com>
2017-01-15 10:00:11 +0800

    Initial React Components.

--- src/index.js
+++ src/index.js
@@ -1,35 +1,132 @@
-import { createStore } from 'redux'
-import * as actions from './actions'
-import todos from './reducers'
+import React, { Component, PropTypes } from 'react'
+import { render } from 'react-dom'
 
-const initialState = {
-    dummy: 'This doesnt matter, this will be overwritten by the reducer.'
+class TodoItem extends Component {
+    _onClick = (e) => {
+        this.props.onTodoClick(this.props.id)
+    }
+
+    render() {
+        const { id, text } = this.props;
+        return <li onClick={this._onClick}>
+                {text}
+            </li>
+    }
 }
-const store = createStore(todos, initialState)
 
-// Let's get the initial state
-console.log('Initial State: ', store.getState())
+TodoItem.propType = {
+    id: PropTypes.number.isRequired,
+    complete: PropTypes.bool.isRequired,
+    text: PropTypes.string.isRequired
+}
 
-// Every time the state changes, let's log it
-// Note that `subscribe()` returns a function for unregistering the listeners
+class TodoList extends Component {
+    _onTodoClick(id) {
+        console.log(id)
+    }
 
-const unsubcribeSiri = store.subscribe(() => {
-    console.log('This is our current State sir!')
-})
+    render() {
+        const { todos } = this.props;
+        return <ul>
+            {todos.map(todo =>
+                <TodoItem
+                    key={todo.id}
+                    {...todo}
+                    onTodoClick={this._onTodoClick}
+                />
+            )}
+        </ul>
+    }
+}
 
-// We can also have multiple subscription
-const unsubscribeLogging = store.subscribe(() => {
-    console.log(store.getState())
-})
+TodoList.propType = {
+    todos: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        complete: PropTypes.bool.isRequired,
+        text: PropTypes.string.isRequired
+    }).isRequired)
+}
 
+class FilterLink extends Component {
+    _onClick = () => {
+        this.props.handleFilter(this.props.filter)
+    }
 
-// Dispatch actions
-store.dispatch(actions.addTodo('Learn about actions'))
-store.dispatch(actions.addTodo('Learn about reducers'))
-store.dispatch(actions.addTodo('Learn about store'))
+    render() {
+        return <a 
+            href="#" 
+            onClick={this._onClick}
+        >{this.props.children}</a>
+    }
+}
 
-// Stop listening to subscriptions
-// The order doesn't matter
-unsubcribeSiri()
-unsubscribeLogging()
+class Footer extends Component {
+    _handleFilter = (filter) => {
+        console.log(filter)
+    }
+
+    render() {
+        return <div>
+            <FilterLink filter="SHOW_COMPLETED" handleFilter={this._handleFilter}>
+                Show Completed
+            </FilterLink>{", "}
+            <FilterLink filter="SHOW_ACTIVE" handleFilter={this._handleFilter}>
+                Show Active
+            </FilterLink>{", "}
+            <FilterLink filter="SHOW_ALL" handleFilter={this._handleFilter}>
+                Show All
+            </FilterLink>
+        </div>
+    }
+}
+
+class AddTodo extends Component {
+    _handleSubmit = (e) => {
+        e.preventDefault();
+        // Add to do here.
+        console.log(this.refs.todo.value);
+    }
+
+    render() {
+        return <form onSubmit={this._handleSubmit}>
+            <input
+                type="text"
+                ref="todo"
+            />
+            <input
+                type="submit" value="Add"
+            />
+        </form>
+    }
+}
+
+class App extends Component {
+    render() {
+        return <div>
+            <AddTodo />
+            <TodoList
+                {...this.props.todos}
+            />
+            <Footer />
+        </div>
+    }
+}
+
+const todos = {
+    visibilityFilter: "SHOW_ALL",
+    todos: [
+        {
+            id: 1,
+            text: 'Integrate Redux to React',
+            completed: false
+        },
+        {
+            id: 2,
+            text: 'Get Food',
+            completed: false
+        }
+    ]
+}
+
+render(<App todos={todos}/>, document.getElementById('app'))
 
